904f87d7965f693db1045095fe19e651f206128f
zelin_mbp24 <zangzelin@gmail.com>
2024-11-18 11:09:55 +0800

    auto push

--- example/nhpca.py
+++ example/nhpca.py
@@ -125,4 +125,4 @@ ax_3.spines[:].set_color('black'); ax_3.spines[:].set_linewidth(1.5)
 
 # Final layout adjustments and save the figure
 plt.tight_layout()
-plt.savefig('pbmc.png', dpi=300)
+plt.savefig('nhpca.png', dpi=300)
